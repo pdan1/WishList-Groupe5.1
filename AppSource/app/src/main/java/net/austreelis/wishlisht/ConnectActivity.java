0079a95b5e554728afbd7cae2543b5391198f9e7
Morgane Leclerc <MorganPingu@users.noreply.github.com>
2020-03-27 04:14:07 +0000

    Add OnClickListener for loginButton
    Signed-off-by: Morgane Leclerc <MorganPingu@users.noreply.github.com>

--- AppSource/app/src/main/java/net/austreelis/wishlisht/ConnectActivity.java
+++ AppSource/app/src/main/java/net/austreelis/wishlisht/ConnectActivity.java
@@ -1,6 +1,9 @@
 package net.austreelis.wishlisht;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -13,5 +16,12 @@ public class ConnectActivity extends AppCompatActivity {
     Toolbar toolbar = findViewById(R.id.bigTitleToolbar);
     setSupportActionBar(toolbar);
 
+    // Handle click on login button
+    findViewById(R.id.connectLoginButton).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Log.d(String.valueOf(R.string.appName), "login button clickety click");
+      }
+    });
   }
 }
