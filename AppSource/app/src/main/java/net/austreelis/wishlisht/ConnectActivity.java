804d33f2fa8c423774ba9eccab435a1b785f94c2
Loïc Grumiaux <loic.grumiaux@student.uclouvain.be>
2020-04-18 15:29:46 +0200

    Add registration activity.

--- AppSource/app/src/main/java/net/austreelis/wishlisht/ConnectActivity.java
+++ AppSource/app/src/main/java/net/austreelis/wishlisht/ConnectActivity.java
@@ -1,6 +1,9 @@
 package net.austreelis.wishlisht;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -9,9 +12,15 @@ public class ConnectActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.connectActivity);
+    setContentView(R.layout.connect_activity);
     Toolbar toolbar = findViewById(R.id.bigTitleToolbar);
     setSupportActionBar(toolbar);
 
   }
+
+  public void register(View view){
+    Intent intent = new Intent(this, RegisterActivity.class);
+    startActivity(intent);
+  }
+
 }
---
+++ AppSource/app/src/main/java/net/austreelis/wishlisht/RegisterActivity.java
@@ -0,0 +1,74 @@
+package net.austreelis.wishlisht;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private String login, email, password1, password2, emailCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity_base_info);
+        Toolbar toolbar = findViewById(R.id.bigTitleToolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void provideBaseInfo(View view){
+
+        this.login = ((EditText) findViewById(R.id.registerLoginField)).getText().toString();
+        this.email = ((EditText) findViewById(R.id.registerEmailField)).getText().toString();
+
+        if(this.login.equals("")||this.email.equals("")){
+            this.generateError(R.string.errorMandatory);
+            return;
+        }
+
+        setContentView(R.layout.register_activity_email_check);
+
+    }
+
+    public void provideEmailCheckCode(View view){
+
+        // Code logic missing, need for a imap server
+        // Faking success
+        this.emailCode = ((EditText) findViewById(R.id.emailCheckCodeField)).getText().toString();
+        if(this.emailCode.equals("")){
+            this.generateError(R.string.errorMandatory);
+            return;
+        }
+        setContentView(R.layout.register_activity_pass);
+
+    }
+
+    public void providePasswords(View view){
+
+        this.password1 = ((EditText) findViewById(R.id.registerFirstPasswordField)).getText().toString();
+        this.password2 = ((EditText) findViewById(R.id.registerSecondPasswordField)).getText().toString();
+
+        if(this.password1.equals("")){
+            this.generateError(R.string.errorMandatory);
+            return;
+        }
+
+        if(!this.password1.equals(this.password2)){
+            this.generateError(R.string.errorNoMatch);
+            return;
+        }
+
+        setContentView(R.layout.register_activity_pass);
+
+    }
+
+    private void generateError(int errorMsg){
+        TextView dialogBox = (TextView)findViewById(R.id.dialogBox);
+        dialogBox.setText(errorMsg);
+        dialogBox.setTextColor(getResources().getColor(R.color.error));
+    }
+}
